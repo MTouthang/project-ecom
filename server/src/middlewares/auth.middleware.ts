b3ac2776a669677a866afbaa940ed551f009a3d7
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-10-14 13:47:11 +0530

    feat: check auth route middleware

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -2,18 +2,14 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import asyncHandler from "./asyncHandler.middleware";
 import CustomError from "../utils/customError.utils";
 import { IDecodedJwtPayload } from "types";
-
-
-
+import { RequestHandler } from "express";
 
 // check for loggedIn
 export const isLoggedIn = asyncHandler( async ( req, res, next ) => {
-
   // get token
   // TODO: make sure proper only jwt token is extracted
-  const  token: string |JwtPayload = (req.headers && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) ? req?.headers?.authorization?.split(" ")[1] : ""
+  const  token: string | JwtPayload = (req.headers && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) ? req?.headers?.authorization?.split(" ")[1] : ""
 
-  console.log("Token -- ", token)
 
   // check for token 
   if(!token) {
@@ -28,13 +24,20 @@ export const isLoggedIn = asyncHandler( async ( req, res, next ) => {
   req.user = decode
   next()
 
+
 })
 
 // authorize roles ---
+interface userRoles {
+  role: number
+}
+
 type IRoles = (number | undefined)[]
 // using rest parameters syntax to take infinite number of arguments 
-export const authorizeRoles = (...roles:IRoles ) => asyncHandler (async (req, res, next) => {
-  if(!roles.includes(req.user?.role)) {
+export const authorizeRoles = (...roles:IRoles ):RequestHandler => asyncHandler (async (req, _res, next) => {
+  const user = req.user as userRoles
+
+  if(!roles.includes(user.role)) {
     return next(
       new CustomError("Your not authorized to access this route ", 403)
     )
