793d00d79f5196f139b2572a0ecd7e7e2070393b
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-10-22 12:30:44 +0530

    Refactor: user_id to _id

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,11 +2,11 @@ import {Request, Response, NextFunction } from "express"
 import asyncHandler from "../middlewares/asyncHandler.middleware"
 import User from "../models/User.model"
 import CustomError from "../utils/customError.utils"
-import os from "os"
-import formidable from 'formidable'
 import cloudinary from "cloudinary"
-import { ObjectId } from "mongoose"
 import { mailHelper } from "../utils/mailHelper.utils"
+import { ObjectId } from "mongoose"
+import { IUser } from "types"
+
 
 
 /**
@@ -37,9 +37,9 @@ export const getUsers = asyncHandler( async (req:Request, res:Response, next:Nex
  * @ACCESS admin only
  */
 export const getUserById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const {userId} = req.params 
+  const {_id} = req.params 
   
-  const user = await User.findById(userId)
+  const user = await User.findById(_id)
 
   if(!user){
     return next(new CustomError("Invalid User Id or User does not exist", 400))
@@ -59,86 +59,11 @@ export const getUserById = asyncHandler(async (req: Request, res: Response, next
  * @return single user with updated user
  * @ACCESS private particular user
  */
-export const updateUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    
-    const {userId} = req.params
-    const form = formidable({
-      keepExtensions: true,
-      uploadDir: os.tmpdir(),
-      maxFileSize: 50 * 1024 * 1024, // 5MB
-    });
-
-     form.parse(req, async (err, fields, files ) => {
-      
-      if (err) {
-        return next(new CustomError(err || 'Something went wrong', 500));
-      }
-
-      try {
-        const user = await User.findByIdAndUpdate(
-          userId,
-          {
-            $set: fields,
-          },
-          {
-            new: true,
-          },
-        ); 
-
-      
-        if (!user) {
-          return next(new CustomError('Error updating user, please try again', 400));
-        }
-
-        if (files && files.userImage) {
-          try {
-            await cloudinary.v2.uploader.destroy(user.avatar.public_id)
-            
-            const incomingFile = files.userImage
-            
-            const persistentFile = incomingFile[0]
-
-          //  only single file
-            if (persistentFile?.filepath.length > 0) {
-              const result = await cloudinary.v2.uploader.upload(
-                persistentFile.filepath,
-                {
-                  folder: 'ecom/users',
-                },
-              );
-
-              if (result) {
-                user.avatar.public_id = result.public_id;
-                user.avatar.secure_url = result.secure_url;
-              }
-            }
-          } catch (error) {
-            return next(new CustomError('Image could not be uploaded', 400));
-          }
-        }
-
-        await user.save();
-
-        res.status(200).json({
-          success: true,
-          message: 'User updated successfully',
-          user,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        return next(
-          new CustomError(
-            error ||
-              'Something went wrong while updating the user, please try again',
-            500,
-          ),
-        );
-      }
-    });
-  
-  },
-);
+// export const updateUser = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const {_id} =req.user as IUser
+//   } 
+// );
 
 /**
  * @CHANGE_PASSWORD 
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { changePassword, deleteUser, getUserById, getUsers, updateUser } from "../controllers/user.controller";
+import { changePassword, deleteUser, getUserById, getUsers,  } from "../controllers/user.controller";
 import { authorizeRoles, isLoggedIn } from "../middlewares/auth.middleware";
 const router = Router()
 
 
 router.route("/users").get(getUsers)
-router.route("/users/:userId").get(getUserById)
-router.route("/users/:userId").put(updateUser)
+router.route("/users/:_id").get(getUserById)
+router.route("/users/me").put()
 // change password 
 router.route("/user/change-password").put(isLoggedIn, changePassword)
 // delete user 
-router.route("/user/:userId/delete").delete(isLoggedIn, authorizeRoles(101), deleteUser)
+router.route("/user/:_id/delete").delete(isLoggedIn, authorizeRoles(101), deleteUser)
 
 export default router
