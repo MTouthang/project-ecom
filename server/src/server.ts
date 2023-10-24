5df60b58f0bd8dd4384e1ab2b79c7264b3693ee2
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-10-24 19:13:37 +0530

    feat: containerise docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import dbConnection from "./configs/db.config";
 
-const PORT = process.env.PORT || 500;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`App is up and running at http://localhost:${PORT}`);
